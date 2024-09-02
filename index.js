b5c519b311b18682eb28c8a06a22f5316b1e52f3
Lucas Wasilewski <66091116+WasixXD@users.noreply.github.com>
2024-09-02 10:04:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import  express from "express"
+import express from "express"
+import cors from "cors"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 const BD = new Map()
 
 
